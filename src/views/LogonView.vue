19de396084f3c53dc1ca9e3a87cf4f8e90f09471
wpp <357281221@qq.com>
2022-10-18 11:40:00 +0800

    检查是否输入内容和两次密码是否一致

--- src/views/LogonView.vue
+++ src/views/LogonView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import type { FirmInstance } from 'element-plus'
+import { ref, reactive } from 'vue'
 const phoneNumber = ref('')
 const jobNumber = ref('')
 const name = ref('')
@@ -16,7 +17,75 @@ const shadowGroup = ref([
 const getCssVarName = (type: string) => {
     return `--el-box-shadow${type ? '-' : ''}${type}`
 }
-const passwordReg = /^[a-zA-Z](?![a-zA-Z]+$)\w{5,19}$/
+const ruleFormRef = ref<FormInstance>()
+
+const checkPhoneNumber = (rule: any, value: any, callback: any) => {
+    if (!value) {
+        return callback(new Error('请输入手机号'))
+    } else if (value.length < 11) {
+        return callback(new Error('请输入正确的手机号'))
+    }
+    callback()
+
+
+}
+
+const validatePass = (rule: any, value: any, callback: any) => {
+    if (value === '') {
+        callback(new Error('请输入密码'))
+    } else {
+        if (ruleForm.checkPass !== '') {
+            if (!ruleFormRef.value) return
+            ruleFormRef.value.validateField('checkPass', () => null)
+        }
+        callback()
+    }
+}
+const validatePass2 = (rule: any, value: any, callback: any) => {
+    if (value === '') {
+        callback(new Error('请再次输入密码'))
+    } else if (value !== ruleForm.pass) {
+        callback(new Error("两次密码输入不一致"))
+    } else {
+        callback()
+    }
+}
+const checkUserName = (rule: any, value: any, callback: any) => {
+    if (value === '') {
+        callback(new Error('请输入用户名'))
+    }
+}
+
+const ruleForm = reactive({
+    pass: '',
+    checkPass: '',
+    phoneNumber: '',
+    userName: ''
+})
+
+const rules = reactive({
+    pass: [{ validator: validatePass, trigger: 'blur' }],
+    checkPass: [{ validator: validatePass2, trigger: 'blur' }],
+    phoneNumber: [{ validator: checkPhoneNumber, trigger: 'blur' }],
+    userName: [{ validator: checkUserName, trigger: 'blur' }]
+})
+
+const submitForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+    formEl.validate((valid) => {
+        if (valid) {
+            console.log('submit!')
+        } else {
+            console.log('error submit!')
+            return false
+        }
+    })
+}
+
+const resetForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+    formEl.resetFields()
+}
 </script>
 
 <template>
@@ -25,14 +94,23 @@ const passwordReg = /^[a-zA-Z](?![a-zA-Z]+$)\w{5,19}$/
           boxShadow: `var(${getCssVarName(shadow.type)})`,
         }">
             <h3>用户注册</h3>
-            <el-input class="jobNumber" v-model="jobNumber" placeholder="请输入工号" clearable />
-            <el-input class="name" v-model="name" placeholder="请输入用户名" clearable />
-            <el-input class="password" v-model="password" type="password" placeholder="请输入密码" clearable show-password />
-            <el-input class="confirmPassword" v-model="confirmPassword" type="password" placeholder="请再次输入密码" clearable
-                show-password />
-            <el-input class="phoneNumber" v-model="phoneNumber" placeholder="请输入手机号" clearable />
+            <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" class="demo-ruleForm">
+                <el-form-item class="username" prop="userName">
+                    <el-input v-model="ruleForm.userName" placeholder="请输入用户名" clearable />
+                </el-form-item>
+                <el-form-item class="password" prop="pass">
+                    <el-input v-model="ruleForm.pass" type="password" placeholder="请输入密码" clearable show-password />
+                </el-form-item>
+                <el-form-item class="confirmPassword" prop="checkPass">
+                    <el-input v-model="ruleForm.checkPass" type="password" placeholder="请再次输入密码" clearable
+                        show-password />
+                </el-form-item>
+                <el-form-item class="phoneNumber" prop="phoneNumber">
+                    <el-input v-model="ruleForm.phoneNumber" type="text" placeholder="请输入手机号" clearable />
+                </el-form-item>
+            </el-form>
             <div class="btn">
-                <el-button class="submit" size="large" type="danger">注册</el-button>
+                <el-button class="submit" size="large" type="danger" @click="submitForm(ruleFormRef)">注册</el-button>
                 <el-button class="toLogon" size="large" type="danger">去登录</el-button>
             </div>
         </div>
@@ -56,7 +134,7 @@ const passwordReg = /^[a-zA-Z](?![a-zA-Z]+$)\w{5,19}$/
     width: 360px;
 }
 
-.name,
+.username,
 .password,
 .confirmPassword,
 .jobNumber,
@@ -76,4 +154,7 @@ h3 {
     justify-content: space-around;
 }
 
+::v-deep .el-form-item {
+    margin: 0;
+}
 </style>
