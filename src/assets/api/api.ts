25c7745446c4db2d2083e3c54898c60ab64d0c15
hxr <1481991555@qq.com>
2022-10-19 09:00:27 +0800

    接口

--- src/assets/api/api.ts
+++ src/assets/api/api.ts
@@ -1,7 +1,19 @@
 import axios from "./http";
 
 export default{
+     /**
+     * 登录接口**/
     loginApi(payload={}){
         return axios.post('/user/login',payload)
+    },
+     /**
+     * 获取请假列表接口**/
+    leaveListApi(payload={}){
+        return axios.post('/user/leave/list',payload)
+    },
+    /**
+     * 请假申请接口**/
+    leaveCreateApi(payload={}){
+        return axios.post('/user/leave/create',payload)
     }
 }
