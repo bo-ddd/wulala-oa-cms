22bff55010c215b338690f29bd14150a595011c8
wpp <357281221@qq.com>
2022-10-19 19:23:15 +0800

    解决点击注册按钮报错

--- src/views/LogonView.vue
+++ src/views/LogonView.vue
@@ -1,39 +1,36 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
-import type { FormInstance } from 'element-plus'
 import { useRouter } from 'vue-router'
+import type { FormInstance } from 'element-plus'
+import axios from '@/assets/api/api'
 
 const ruleFormRef = ref<FormInstance>()
+
 let router = useRouter();
 const toLogin = function () {
     router.push('/')
 }
-const shadowGroup = ref([
-    {
-        name: 'Basic Shadow',
-        type: '',
-    }
-])
-const getCssVarName = (type: string) => {
-    return `--el-box-shadow${type ? '-' : ''}${type}`
-}
-
-const checkPhoneNumber = (rule: any, value: any, callback: any) => {
-    if (!value) {
-        return callback(new Error('请输入手机号'))
-    } else if (value.length < 11) {
-        return callback(new Error('请输入正确的手机号'))
+const checkAvatarName = (rule: any, value: any, callback: any) => {
+    if (/[!0-9]/.test(value)) {
+        callback(new Error('您输入的用户名不符合规范'))
+    } else if (value.length < 5 || value.length > 20) {
+        callback(new Error('用户名长度为4-20位'))
     }
     callback();
 }
-
 const checkUserName = (rule: any, value: any, callback: any) => {
     if (value === '') {
-        return callback(new Error('请输入用户名'))
+        callback(new Error('请输入用户名'))
     } else if (/[!0-9]/.test(value)) {
-        return callback(new Error('输入的用户名不符合规范'))
-    } else if (value.length < 4 || value.length > 20) {
-        return callback(new Error('用户名长度为4-20位'))
+        callback(new Error('您输入的用户名不符合规范'))
+    } else if (value.length < 5 || value.length > 20) {
+        callback(new Error('用户名长度为4-20位'))
+    }
+    callback();
+}
+const checkPhoneNumber = (rule: any, value: any, callback: any) => {
+    if (value === '') {
+        return callback(new Error('请输入手机号'))
     }
     callback();
 }
@@ -52,10 +49,9 @@ const validatePass2 = (rule: any, value: any, callback: any) => {
     if (value === '') {
         callback(new Error('请再次输入密码'))
     } else if (value !== ruleForm.pass) {
-        callback(new Error("两次密码输入不一致"))
-    } else {
-        callback()
+        callback(new Error("两次输入密码不一致!"))
     }
+    callback();
 }
 
 const ruleForm = reactive({
@@ -63,6 +59,7 @@ const ruleForm = reactive({
     checkPass: '',
     phoneNumber: '',
     userName: '',
+    avatarName: '',
 })
 
 const rules = reactive({
@@ -70,32 +67,42 @@ const rules = reactive({
     checkPass: [{ validator: validatePass2, trigger: 'blur' }],
     phoneNumber: [{ validator: checkPhoneNumber, trigger: 'blur' }],
     userName: [{ validator: checkUserName, trigger: 'blur' }],
+    avatarName: [{ validator: checkAvatarName, trigger: 'blur' }],
+
 })
 
 const submitForm = (formEl: FormInstance | undefined) => {
     if (!formEl) return
     formEl.validate((valid) => {
         if (valid) {
-            console.log('submit!')
+            console.log('submit!');
+            (async function () {
+                let userRegister = await axios.userRegisterApi({
+                    pass: ruleForm.pass,
+                    checkPass: ruleForm.checkPass,
+                    phoneNumber: ruleForm.phoneNumber,
+                    userName: ruleForm.userName,
+                    avatarName: ruleForm.avatarName
+                })
+            })()
         } else {
             console.log('error submit!')
             return false
         }
     })
 }
-
-
 </script>
 
 <template>
     <div class="logon">
-        <div class="box" v-for="(shadow, i) in shadowGroup" :key="i" :style="{
-          boxShadow: `var(${getCssVarName(shadow.type)})`,
-        }">
+        <div class="box" :style="{ boxShadow:'--el-box-shadow-dark',}">
             <h3>用户注册</h3>
             <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" class="demo-ruleForm">
                 <el-form-item class="username" prop="userName">
-                    <el-input v-model="ruleForm.userName" placeholder="请输入用户名" clearable />
+                    <el-input v-model="ruleForm.userName" type="text" placeholder="请输入用户名" clearable />
+                </el-form-item>
+                <el-form-item class="avatarName" prop="avatarName">
+                    <el-input v-model="ruleForm.avatarName" type="text" placeholder="请输入昵称(非必填)" clearable />
                 </el-form-item>
                 <el-form-item class="password" prop="pass">
                     <el-input v-model="ruleForm.pass" type="password" placeholder="请输入密码" clearable show-password />
@@ -105,15 +112,13 @@ const submitForm = (formEl: FormInstance | undefined) => {
                         show-password />
                 </el-form-item>
                 <el-form-item class="phoneNumber" prop="phoneNumber">
-                    <el-input v-model.number="ruleForm.phoneNumber" type="text" placeholder="请输入手机号" clearable />
+                    <el-input v-model.number="ruleForm.phoneNumber" placeholder="请输入手机号" clearable />
                 </el-form-item>
-                <el-form-item class="btn">
-                    <el-button class="submit" size="large" type="danger" @click="submitForm(ruleFormRef)">注册
-                    </el-button>
-                    <el-button class="toLogin" size="large" type="danger" @click="toLogin()">去登录</el-button>
+                <el-form-item>
+                    <el-button type="danger" @click="submitForm(ruleFormRef)">注册</el-button>
+                    <el-button type="danger" @click="toLogin">去登录</el-button>
                 </el-form-item>
             </el-form>
-
         </div>
     </div>
 </template>
@@ -137,6 +142,7 @@ const submitForm = (formEl: FormInstance | undefined) => {
 }
 
 .username,
+.avatarName,
 .password,
 .confirmPassword,
 .phoneNumber,
