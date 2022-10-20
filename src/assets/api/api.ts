a45e1b9819e727f802e7e8cf1be0370fd1da0f57
hxr <1481991555@qq.com>
2022-10-20 20:02:37 +0800

    修改

--- src/assets/api/api.ts
+++ src/assets/api/api.ts
@@ -1,32 +1,36 @@
 import axios from "./http";
 //用户
-interface userRegister{
-    username:string;
-    password:string
-    avatarName?:string 
-    phoneNumber:string
+interface userRegister {
+    username: string;
+    password: string
+    avatarName?: string
+    phoneNumber: string
 }
 
-export default{
-     /**
-     * 登录接口**/
-    loginApi(payload={}){
-        return axios.post('/user/login',payload)
+export default {
+    /**
+    * 登录接口**/
+    loginApi(payload = {}) {
+        return axios.post('/user/login', payload)
     },
-     /**
-     * 获取请假列表接口**/
-    leaveListApi(payload={}){
-        return axios.post('/user/leave/list',payload)
+    /**
+    * 获取请假列表接口**/
+    getLeaveListApi(payload = {}) {
+        return axios.post('/user/leave/list', payload)
     },
     /**
      * @description  * 请假申请接口**
- ***/
-    leaveCreateApi(payload={}){
-        return axios.post('/user/leave/create',payload)
+ * **/
+    leaveCreateApi(payload = {}) {
+        return axios.post('/user/leave/create', payload)
     },
     /**
-     * 用户注册接口**/
-     userRegisterApi(payload:userRegister){
-        return axios.post('/user/register',payload)
+     * 用户注册接口*
+     * "username": //用户名，6-20位之间
+     * "password":  //密码，6-20位之间
+     *  avatarName": //昵称，1-12位之间
+     *  "phoneNumber":  //手机号*/
+    userRegisterApi(payload: userRegister) {
+        return axios.post('/user/register', payload)
     }
 }
