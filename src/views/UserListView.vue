e182d4f74bf57e2707a605fcef9bdc523f6f40e6
wpp <357281221@qq.com>
2022-10-20 20:38:44 +0800

    用户列表页面

--- src/views/UserListView.vue
+++ src/views/UserListView.vue
@@ -1,60 +1,76 @@
 <template>
-    <div>
-        <div class="ipt">
-            <span class="label">查询：</span>
-            <el-input v-model="input" size="small" placeholder="请输入用户ID" />
-            <el-button type="danger" size="small">搜索</el-button>
-        </div>
-        <el-table :data="tableData" border style="width: 100%" fit>
-            <el-table-column prop="name" label="用户名" align='center' />
+
+    <div class="ipt">
+        <span class="label">查询：</span>
+        <el-input v-model="input" size="small" placeholder="请输入用户ID" />
+        <el-button type="danger" size="small" @click="userSearch(input)">搜索</el-button>
+    </div>
+    <el-table :data="userListData" border style="width: 100%" fit>
+        <el-table-column label="用户ID" align="center">
+            <template #default="scope">
+                <div>{{ scope.row.userId }}</div>
+            </template>
+        </el-table-column>
+        <el-table-column label="用户昵称" align="center">
+            <template #default="scope">
+                <div>{{ scope.row.avatarName }}</div>
+            </template>
+        </el-table-column>
+
+        <el-table-column label="联系方式" align="center">
+            <template #default="scope">
+                <div>{{ scope.row.phoneNumber }}</div>
+            </template>
+        </el-table-column>
+        <!-- <el-table-column prop="userId" label="用户名" align='center' />
             <el-table-column prop="address" label="用户身份" align='center' />
             <el-table-column prop="phoneNumber" label="手机号" align='center' />
             <el-table-column prop="sex" label="性别" align='center' />
-            <el-table-column prop="aaa" label="状态" align='center' />
-            <el-table-column prop="date" label="入职时间" align='center' />
-            <el-table-column label="操作" width="300" align='center'>
-                <template #default="scope">
-                    <el-button size="small">修改信息</el-button>
-                    <el-button size="small" type="danger">删除
-                    </el-button>
-                </template>
-            </el-table-column>
-        </el-table>
-    </div>
+            <el-table-column prop="aaa" label="状态" align='center' /> 
+            <el-table-column prop="date" label="入职时间" align='center' /> -->
+        <el-table-column label="操作" width="300" align='center'>
+            <template #default="scope">
+                <el-button size="small">修改信息</el-button>
+                <el-button size="small" type="danger" @click="userDelete(scope.$index, scope.row)">删除
+                </el-button>
+            </template>
+        </el-table-column> -->
+    </el-table>
+
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-const input = ref('')
+import axios from '@/assets/api/api'
+const input = ref();
 
-const tableData = [
-    {
-        date: '2020-2-2',
-        name: 'Tom',
-        address: '组员',
-        phoneNumber: '13333333333',
-        sex: '1',
-        aaa: '在岗'
-    },
-    {
-        date: '2022-10-1',
-        name: 'Bob',
-        address: '组长',
-        phoneNumber: '11111111111',
-        sex: '0',
-        aaa: '请假'
-    },
-    {
-        date: '',
-        name: 'Tom',
-        address: '',
-    },
-    {
-        date: '',
-        name: 'Tom',
-        address: '',
-    },
-]
+
+
+let userListData = ref();
+(async function () {
+    let userList = await axios.userListApi({})
+    userListData.value = userList.data.list
+})()
+interface User {
+    userId: number
+    avatarName: string
+    phoneNumber: number
+}
+
+const userDelete = (index: number, row: User) => {
+    console.log(index, row)
+}
+// let userInfoData = ref();
+
+const userSearch = function(userId: any) {
+    console.log('------------userId------------');
+    console.log(userId)
+    console.log('------------ipt--------');
+    console.log(input.value);
+    (async function () {
+        await axios.userInfoApi(userId)
+    })()
+}
 </script>
 
 <style scoped>
@@ -72,6 +88,7 @@ const tableData = [
 :deep(.el-input) {
     width: 200px;
 }
+
 :deep(.el-button) {
     margin-left: 20px;
 }
