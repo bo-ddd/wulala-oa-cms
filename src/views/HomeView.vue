fe4454097730213ea2a9699141bc077b75beb1f6
MGX <272773670@qq.com>
2022-10-28 20:33:11 +0800

    优化轮播图

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,36 @@
 import * as echarts from 'echarts';
 import { onMounted } from 'vue';
 
-//班费排行榜;
+const bannerList=[
+  {
+    id:1,
+    url:'https://tse1-mm.cn.bing.net/th/id/OIP-C.Oiy0ZhrBtSBpBjAkDAR7xAHaEo?w=289&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+  },
+  {
+    id:2,
+    url:'https://tse4-mm.cn.bing.net/th/id/OIP-C.fFUMf4Nfh3EXVe7hA8No-QHaEo?w=289&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+  },
+  {
+    id:3,
+    url:'https://tse3-mm.cn.bing.net/th/id/OIP-C.D25iBDGx4ddYgqmHZMqmRQHaEo?w=283&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+  },
+  {
+    id:4,
+    url:'https://tse2-mm.cn.bing.net/th/id/OIP-C.or2yOxvfgmQgKISOKZEFDQHaEo?w=240&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+  },
+  {
+    id:5,
+    url:'https://tse2-mm.cn.bing.net/th/id/OIP-C.Nxv-KX874ocnv1CjVApVmwHaEo?w=279&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+  },
+  {
+    id:6,
+    url:'https://tse4-mm.cn.bing.net/th/id/OIP-C.XQELBq9MyEiiRiSc8OoTwwHaEK?w=271&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+  }
+]
+
 
 onMounted(() => {
+  //班费排行榜;
   type EChartsOption = echarts.EChartsOption;
   let myChart = echarts.init(document.getElementById('fund')!);
 
@@ -46,8 +73,6 @@ onMounted(() => {
   };
 
   option && myChart.setOption(option);
-
-
 })
 
 
@@ -60,24 +85,30 @@ onMounted(() => {
     <el-row :gutter="50">
       <el-col :span="16">
         <div class="grid-content ep-bg-purple cyan">
+
            <!-- 轮播图 -->
           <el-carousel :interval="4000" type="card" height="200px">
-            <el-carousel-item v-for="item in 6" :key="item">
-              <h3 text="2xl" justify="center">{{ item }}</h3>
+            <el-carousel-item v-for="item in bannerList" :key="item.id">
+                <img :src="item.url" style="width:100%">
             </el-carousel-item>
           </el-carousel>
+
         </div>
       </el-col>
       <el-col :span="8">
         <div class="grid-content ep-bg-purple grid-fund">
+
           <!-- 班费扇形图 -->
           <div id="fund" class="pt-20"></div>
+
         </div>
       </el-col>
     </el-row>
     <el-row :gutter="20">
       <el-col :span="8">
-        <div class="grid-content ep-bg-purple green"></div>
+        <div class="grid-content ep-bg-purple green">
+          ahfoanfa
+        </div>
       </el-col>
       <el-col :span="8">
         <div class="grid-content ep-bg-purple color1 "></div>
@@ -135,5 +166,7 @@ onMounted(() => {
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
 }
-
+.green{
+  background-color: green;
+}
 </style>
