637297ac16c52f8351493beabde7455cfc5604cd
MGX <272773670@qq.com>
2022-10-31 19:59:02 +0800

    初始化查看权限详情页面

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,34 +3,84 @@
 <script lang="ts" setup>
 import * as echarts from 'echarts';
 import { onMounted } from 'vue';
+import { Check } from '@element-plus/icons-vue'
+// 进度条组件自带方法;
 
-const bannerList=[
+const bannerList = [
   {
-    id:1,
-    url:'https://tse1-mm.cn.bing.net/th/id/OIP-C.Oiy0ZhrBtSBpBjAkDAR7xAHaEo?w=289&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+    id: 1,
+    url: 'https://tse1-mm.cn.bing.net/th/id/OIP-C.Oiy0ZhrBtSBpBjAkDAR7xAHaEo?w=289&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
   },
   {
-    id:2,
-    url:'https://tse4-mm.cn.bing.net/th/id/OIP-C.fFUMf4Nfh3EXVe7hA8No-QHaEo?w=289&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+    id: 2,
+    url: 'https://tse4-mm.cn.bing.net/th/id/OIP-C.fFUMf4Nfh3EXVe7hA8No-QHaEo?w=289&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
   },
   {
-    id:3,
-    url:'https://tse3-mm.cn.bing.net/th/id/OIP-C.D25iBDGx4ddYgqmHZMqmRQHaEo?w=283&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+    id: 3,
+    url: 'https://tse3-mm.cn.bing.net/th/id/OIP-C.D25iBDGx4ddYgqmHZMqmRQHaEo?w=283&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
   },
   {
-    id:4,
-    url:'https://tse2-mm.cn.bing.net/th/id/OIP-C.or2yOxvfgmQgKISOKZEFDQHaEo?w=240&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+    id: 4,
+    url: 'https://tse2-mm.cn.bing.net/th/id/OIP-C.or2yOxvfgmQgKISOKZEFDQHaEo?w=240&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
   },
   {
-    id:5,
-    url:'https://tse2-mm.cn.bing.net/th/id/OIP-C.Nxv-KX874ocnv1CjVApVmwHaEo?w=279&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+    id: 5,
+    url: 'https://tse2-mm.cn.bing.net/th/id/OIP-C.Nxv-KX874ocnv1CjVApVmwHaEo?w=279&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
   },
   {
-    id:6,
-    url:'https://tse4-mm.cn.bing.net/th/id/OIP-C.XQELBq9MyEiiRiSc8OoTwwHaEK?w=271&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+    id: 6,
+    url: 'https://tse4-mm.cn.bing.net/th/id/OIP-C.XQELBq9MyEiiRiSc8OoTwwHaEK?w=271&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
   }
 ]
 
+const cleanerList = [
+  {
+    id: 1,
+    name: ["卫正阳", "马格烜"]
+  },
+  {
+    id: 2,
+    name: ["周永峰", "郝永祥"]
+  },
+  {
+    id: 3,
+    name: ["侯鑫茹", "朱江艳"]
+  },
+  {
+    id: 4,
+    name: ["刘伟博", "尚宇豪"]
+  },
+  {
+    id: 5,
+    name: ["宋巍", "申少杰"]
+  },
+  {
+    id: 6,
+    name: ["李展鹏", "魏浩爽"]
+  },
+  {
+    id: 7,
+    name: ["朱鑫鹏", "王鹏程"]
+  }
+]
+
+const projectProgress = [
+  {
+    id: 1,
+    name: '马超项目组',
+    percentage: 20
+  },
+  {
+    id: 2,
+    name: '点餐项目组',
+    percentage: 50
+  },
+  {
+    id: 3,
+    name: 'OA项目组',
+    percentage: 70
+  }
+]
 
 onMounted(() => {
   //班费排行榜;
@@ -85,41 +135,64 @@ onMounted(() => {
     <el-row :gutter="50">
       <el-col :span="16">
         <div class="grid-content ep-bg-purple cyan">
-
-           <!-- 轮播图 -->
+          <!-- 轮播图 -->
           <el-carousel :interval="4000" type="card" height="200px">
             <el-carousel-item v-for="item in bannerList" :key="item.id">
-                <img :src="item.url" style="width:100%">
+              <img :src="item.url" style="width:100%">
             </el-carousel-item>
           </el-carousel>
-
         </div>
       </el-col>
       <el-col :span="8">
         <div class="grid-content ep-bg-purple grid-fund">
-
           <!-- 班费扇形图 -->
           <div id="fund" class="pt-20"></div>
-
         </div>
       </el-col>
     </el-row>
+
     <el-row :gutter="20">
-      <el-col :span="8">
-        <div class="grid-content ep-bg-purple green">
+      <el-col :span="10">
+        <!-- 项目进度条 -->
+        <div class="progress-project">
+          <h3>
+            项目进展情况
+          </h3>
+          <div class="progress ep-bg-purple">
+
+            <div v-for="item in projectProgress" :key="item.id">
+              <!-- 项目完成的样式 -->
+              <el-progress v-if="item.percentage == 100" type="circle" :percentage="100" status="success">
+                <el-button type="success" :icon="Check" circle />
+              </el-progress>
+              <!-- 项目进行中的样式 -->
+              <el-progress v-if="item.percentage != 100" type="dashboard" :percentage="item.percentage">
+                <template #default="{ percentage }">
+                  <span class="percentage-value">{{ percentage }}%</span>
+                  <span class="percentage-label">{{ item.name }}</span>
+                </template>
+              </el-progress>
+            </div>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content ep-bg-purple">
           ahfoanfa
         </div>
       </el-col>
-      <el-col :span="8">
-        <div class="grid-content ep-bg-purple color1 "></div>
+      <el-col :span="4">
+        <div class="grid-content ep-bg-purple">
+          ahfoanfa
+        </div>
       </el-col>
       <el-col :span="4">
-        <div class="grid-content ep-bg-purple color2"></div>
-      </el-col>
-      <el-col :span="4">
-        <div class="grid-content ep-bg-purple color3"></div>
+        <div class="grid-content ep-bg-purple">
+          ahfoanfa
+        </div>
       </el-col>
     </el-row>
+
     <el-row :gutter="20">
       <el-col :span="4">
         <div class="grid-content ep-bg-purple color4"></div>
@@ -166,7 +239,45 @@ onMounted(() => {
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
 }
-.green{
-  background-color: green;
+
+.demo-progress .el-progress--line {
+  margin-bottom: 15px;
+  width: 350px;
+}
+
+/* 进度条 */
+.percentage-value {
+  display: block;
+  margin-top: 10px;
+  font-size: 28px;
+}
+
+.percentage-label {
+  display: block;
+  margin-top: 10px;
+  font-size: 12px;
+}
+
+.demo-progress .el-progress--line {
+  margin-bottom: 15px;
+  width: 350px;
+}
+
+.demo-progress .el-progress--circle {
+  margin-right: 15px;
+}
+
+.progress-project {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+}
+
+.progress {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 126px;
 }
 </style>
--- src/views/RoleEditing.vue
+++ src/views/RoleEditing.vue
@@ -3,9 +3,15 @@ import { reactive, ref, onMounted, watch } from 'vue'
 import axios from "../assets/api/api"
 
 
+interface RolePermission{
+  id:number,
+  permissionId: number,
+  permissionName:string
+}
+
 const value = ref('')
-
-
+const roleId = ref<number>()
+const defaultCheckedKeys=reactive<number[]>([])
 //处理数据结构，为无限极结构;
 interface PermissionList {
   id: number,
@@ -37,9 +43,12 @@ const formatData = reactive<Tree[]>([]);
 
 (async () => {
   Object.assign(roleList, (await axios.getRoleListApi({})).data)
-  console.log(roleList)
   let permissionList = (await axios.getPermissionListApi({})).data
   Object.assign(formatData, formatePermissionList(permissionList))
+  console.log('----------------角色列表--------------------')
+  console.log(roleList)
+  console.log('----------------权限列表--------------------')
+  console.log(formatData)
 })();
 
 //树形数据结构的类型接口;
@@ -50,10 +59,28 @@ interface Tree {
   children: Tree[]
 }
 
-// const getCheckedId = (id: number) => {
-//   console.log(id)
+const getUserPermission = () => {
+  
+ //拿到id;
+  roleList.forEach(item => {
+    if (item.roleName == value.value) {
+      roleId.value = item.id
+    }
+  });
 
-// }
+  //根据id搜索出对应的数据;
+  (async()=>{
+  let {data}= await axios.queryRolePermissionApi({
+      roleId: roleId.value
+    })
+     defaultCheckedKeys.length=0
+     data.forEach((item:RolePermission)=>{
+        defaultCheckedKeys.push(item.permissionId)
+      })
+      console.log('-------------------默认选中的权限数组-------------------')
+      console.log(defaultCheckedKeys)
+  })();
+}
 
 
 
@@ -62,14 +89,21 @@ interface Tree {
 <template>
   <div>
     <div class="top-list mt-10">
-      <el-select v-model="value" filterable placeholder="请输入角色名称" @change="">
+      <el-select v-model="value" filterable placeholder="请输入角色名称" @change="getUserPermission">
         <el-option v-for="item in roleList" :key="item.id" :label="item.roleName" :value="item.roleName" />
       </el-select>
-      <el-button type="danger" size="small">确定</el-button>
+      <el-button type="danger" size="small">确定{{defaultCheckedKeys}}</el-button>
     </div>
     <div class="custom-tree-node-container mt-10">
-      <el-tree :data="formatData" show-checkbox node-key="id" default-expand-all :expand-on-click-node="false"
-        :props="{ label: 'permissionName' }" />
+      <el-tree :data="formatData" 
+      node-key="id" 
+      default-expand-all
+      show-checkbox
+      :default-checked-keys="defaultCheckedKeys"
+      check-on-click-node
+      :expand-on-click-node="false" 
+      :props="{ label: 'permissionName' }" />
+
     </div>
   </div>
 
