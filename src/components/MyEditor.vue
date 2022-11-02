8f0ad7cd314ee57a78b419ef505ad8f376a5b693
zhudazhuang <1270049837@qq.com>
2022-11-02 08:17:31 +0800

    拉一下

--- src/components/MyEditor.vue
+++ src/components/MyEditor.vue
@@ -1,3 +1,43 @@
+<script setup lang="ts">
+import '@wangeditor/editor/dist/css/style.css' // 引入 css
+import axios from '@/assets/api/api'
+import { onBeforeUnmount, ref, shallowRef, onMounted } from 'vue'
+import { Editor, Toolbar } from '@wangeditor/editor-for-vue';
+
+
+    // 编辑器实例，必须用 shallowRef
+    const editorRef = shallowRef()
+
+    // 内容 HTML
+    const valueHtml = ref('1')
+    console.log(valueHtml.value);
+    
+    // 模拟 ajax 异步获取内容
+    // onMounted(() => {
+    //     setTimeout(() => {
+    //         valueHtml.value = `<p>日报内容</p>`
+    //     }, 1500)
+    // });
+
+    const toolbarConfig = {}
+    const editorConfig = { placeholder: '请输入内容...' }
+
+    // 组件销毁时，也及时销毁编辑器
+    onBeforeUnmount(() => {
+        const editor = editorRef.value
+        if (editor == null) return
+        editor.destroy()
+    })
+
+    const handleCreated = (editor : any) => {
+      editorRef.value = editor // 记录 editor 实例，重要！
+    }
+    
+    let mode = 'default'
+  
+   
+</script>  
+
 <template>
     <div style="border: 1px solid #ccc">
       <Toolbar
@@ -15,50 +55,3 @@
       />
     </div>
 </template>
-<script lang="ts">
-import '@wangeditor/editor/dist/css/style.css' // 引入 css
-
-import { onBeforeUnmount, ref, shallowRef, onMounted } from 'vue'
-import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
-
-export default {
-  components: { Editor, Toolbar },
-  setup() {
-    // 编辑器实例，必须用 shallowRef
-    const editorRef = shallowRef()
-
-    // 内容 HTML
-    const valueHtml = ref('<p>你好！小卡拉米</p>')
-
-    // 模拟 ajax 异步获取内容
-    onMounted(() => {
-        setTimeout(() => {
-            valueHtml.value = `<p>日报内容</p>`
-        }, 1500)
-    })
-
-    const toolbarConfig = {}
-    const editorConfig = { placeholder: '请输入内容...' }
-
-    // 组件销毁时，也及时销毁编辑器
-    onBeforeUnmount(() => {
-        const editor = editorRef.value
-        if (editor == null) return
-        editor.destroy()
-    })
-
-    const handleCreated = (editor : any) => {
-      editorRef.value = editor // 记录 editor 实例，重要！
-    }
-
-    return {
-      editorRef,
-      valueHtml,
-      mode: 'default', // 或 'simple'
-      toolbarConfig,
-      editorConfig,
-      handleCreated
-    };
-  }
-}
-</script>  
