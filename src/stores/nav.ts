b4d2e28b5ef9ad6b72e06498fef1b3f7eececfd3
MGX <272773670@qq.com>
2022-11-08 08:46:20 +0800

    完成动态权限路由渲染

--- src/stores/nav.ts
+++ src/stores/nav.ts
@@ -1,6 +1,36 @@
 import { defineStore } from "pinia";
+import { reactive, computed } from "vue";
+import type { Permission } from "@/types/Permission";
+import axios from "../assets/api/api";
+import type { UserInfo, UserPermissionList } from "@/types/User";
 
 export const useStore = defineStore('counter', () => {
-  let routerFlag: boolean = true
-  return { routerFlag }
+  let userInfo = reactive<UserInfo>({
+    userId: 0
+  })
+  let userPremissionList = reactive<Permission[]>([]);
+  let userId = computed(() => userInfo.userId)
+
+  //获取userId;
+  const getUserId = () => {
+    return axios.queryUserInfoApi().then(res => {
+      setUserId(res.data)
+    })
+  }
+  const setUserId = (payLoad: UserInfo) => {
+    Object.assign(userInfo, payLoad)
+  }
+
+  //获取权限列表;
+  const getUserPermissionList = () => {
+    return axios.permissionUserListApi({ userId: userInfo.userId }).then(res => {
+      setUserPermissionList(res.data)
+    })
+  }
+
+  const setUserPermissionList = (payLoad: UserPermissionList) => {
+    Object.assign(userPremissionList, payLoad)
+  }
+
+  return { userId, userPremissionList, getUserId, getUserPermissionList }
 })
