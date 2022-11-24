f64c45a5f9476ec1c8a87a7ae4febe4d6b7de4a1
hxr <1481991555@qq.com>
2022-11-24 20:48:20 +0800

    新增

--- src/stores/userInfo.ts
+++ src/stores/userInfo.ts
@@ -1,9 +1,11 @@
 import { defineStore } from "pinia";
 import { reactive } from "vue";
 import axios from "../assets/api/api";
+import type { UserInfo } from "@/types/User";
+
 
 export const useUserStore=defineStore('userInfo',()=>{
-      const userInfo=reactive({})
+      const userInfo=reactive({} as UserInfo) 
       const getUserInfo=function(){
           return axios.queryUserInfoApi().then(res=>{
                Object.assign(userInfo,res.data)
